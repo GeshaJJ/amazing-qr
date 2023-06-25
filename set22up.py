0c8f3608ec6a44b33d47f91d5c0c03fdcaed6ace
GeshaJJ <137683428+GeshaJJ@users.noreply.github.com>
2023-06-25 15:44:39 +0700

    Create set22up.py

---
+++ set22up.py
@@ -0,0 +1,9 @@
+amzqr Words
+      [-v {1,2,3,...,40}]
+      [-l {L,M,Q,H}]
+      [-n output-filename]
+      [-d output-directory]
+      [-p picture_file]
+      [-c]
+      [-con contrast]
+      [-bri brightness]
